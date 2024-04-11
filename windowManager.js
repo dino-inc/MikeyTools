49d5d3d4190383fc774a576d2dc4865bcf70c425
Ethan <ethan8960@gmail.com>
2024-04-11 10:51:06 -0700

    updated path to iframe

--- windowManager.js
+++ windowManager.js
@@ -13,5 +13,5 @@ function createHealthSumWindow() {
         animateIn: 'jsPanelFadeIn',
         onwindowresize: true,
     });
-    panel.content.innerHTML = "<iframe src = \"/toolpages/healthSum.html\" style=\" width:100%; height:99%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;\"></iframe>"
+    panel.content.innerHTML = "<iframe src = \"./toolpages/healthSum.html\" style=\" width:100%; height:99%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;\"></iframe>"
 }
