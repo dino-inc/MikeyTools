862d18ed584ec61f5992460b9c92c1a3e657e62f
Ethan <ethan8960@gmail.com>
2024-04-11 10:44:14 -0700

    Converted panel to iframe

--- toolpages/healthSum.js
+++ toolpages/healthSum.js
@@ -1,11 +1,10 @@
-document.getElementsByClassName("equalizeButton")[0].addEventListener("click", equalize());
+document.getElementsByClassName("equalizeButton")[0].addEventListener("click", equalize);
 
 function equalize(){
     let healthPool = document.getElementsByClassName("creature")
     let poolSum = 0;
     for (let i = 0; i < healthPool.length; i++) {
-        poolSum += healthPool[i].value;
+        poolSum += parseInt(healthPool[i].value);
     } 
-    document.getElementsByClassName("output")[0].innerHTML = poolSum/healthPool.length
-    console.log(poolSum/healthPool.length)
+    document.getElementsByClassName("healthSumOutput")[0].innerHTML = poolSum/healthPool.length
 }
--- windowManager.js
+++ windowManager.js
@@ -2,7 +2,7 @@
 document.getElementsByClassName("healthSum")[0].addEventListener ("click", createHealthSumWindow);
 
 function createHealthSumWindow() {
-    jsPanel.create({
+    let panel = jsPanel.create({
         theme: 'dark',
         headerTitle: 'Socialized Healthcare',
         headerToolbar: '<span class="text-sm">Socialism is when your health gets averaged.</span>',
@@ -11,12 +11,7 @@ function createHealthSumWindow() {
             height: () => { return Math.min(500, window.innerHeight*0.6);}
         },
         animateIn: 'jsPanelFadeIn',
-        contentAjax: {
-            url: './toolpages/healthsum.html',
-            done: (xhr, panel) => {
-                panel.content.innerHTML = xhr.responseText;
-            }
-        },
         onwindowresize: true,
     });
+    panel.content.innerHTML = "<iframe src = \"/toolpages/healthSum.html\" style=\" width:100%; height:99%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;\"></iframe>"
 }
