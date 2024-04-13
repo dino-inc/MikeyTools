60f4c008357f89434ce2897bb2e94b3bc6b3e641
Ethan <ethan8960@gmail.com>
2024-04-12 20:48:02 -0700

    Holonet skeleton files
    the basics for the holonet

--- windowManager.js
+++ windowManager.js
@@ -1,5 +1,6 @@
 // Add listeners to the buttons
 document.getElementsByClassName("healthSum")[0].addEventListener ("click", createHealthSumWindow);
+document.getElementsByClassName("holonet")[0].addEventListener ("click", createHolonet);
 document.getElementsByClassName("wannaWanga")[0].addEventListener ("click", createWannaWanga);
 
 function createHealthSumWindow() {
@@ -33,4 +34,21 @@ function createWannaWanga() {
     });
 
     panel.content.innerHTML = "<iframe width=\"560\" height=\"315\" src=\"https:\/\/www.youtube.com/embed/CfTmQGmtJRM?si=OsEoBS7fq_P83vZb&autoplay=1\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>"
+}
+
+function createHolonet() {
+    let panel = jsPanel.create({
+        theme: 'dark',
+        headerTitle: 'Holonet Access Terminal',
+        headerToolbar: '<span class="text-sm">Invading privacy since 20000 BTC.</span>',
+        panelSize: {
+            width: () => { return Math.min(800, window.innerWidth*0.9);},
+            height: () => { return Math.min(600, window.innerHeight*0.6);}
+        },
+        animateIn: 'jsPanelFadeIn',
+        onwindowresize: true,
+    });
+
+
+    panel.content.innerHTML = "<iframe src = \"./toolpages/holonet.html\" style=\" width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999; display: block;\"></iframe>"
 }
